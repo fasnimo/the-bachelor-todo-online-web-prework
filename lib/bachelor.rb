0e09b2772a0fbf180031b99dc7be9bac15e6be42
Rene Cocom <fasnimo@gmail.com>
2019-06-26 01:09:33 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,10 +1,24 @@
+require 'pry'
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |player|
+    if player["status"] == "Winner"
+     return player["name"].split[0]
+    end 
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
-end
+  
+  data.each do |season, array|
+    array.each do |people|
+      people.each do |string, value|
+        if value == "occuptation"
+          return people["name"]
+      end  
+    end
+   end
+  end
+end 
 
 def count_contestants_by_hometown(data, hometown)
   # code here
